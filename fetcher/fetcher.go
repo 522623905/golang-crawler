db3ddad57145c13e27286975aa5ff5e46845a8b7
long <522623905@qq.com>
2018-05-30 10:08:16 +0800

    爬虫，解决卡死现象

---
+++ fetcher/fetcher.go
@@ -0,0 +1,50 @@
+package fetcher
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+)
+
+//提取url内容
+func Fetch(url string) ([]byte, error) {
+	//要增加头，否则爬虫的时候可能会返回403权限错误
+	client := &http.Client{}
+	reqest, err := http.NewRequest("GET", url, nil)
+	reqest.Header.Add("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0")
+	if err != nil {
+		panic(err)
+	}
+	resp, _ := client.Do(reqest)
+
+	//	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := determineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+//自动识别编码格式
+func determineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		log.Printf("Fetcher error: %v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
---
+++ model/profile.go
@@ -0,0 +1,18 @@
+package model
+
+//人物的个人信息数据
+type Profile struct {
+	Name       string
+	Gender     string
+	Age        int
+	Height     int
+	Weight     int
+	Income     string
+	Marriage   string
+	Education  string
+	Occupation string
+	Hokou      string
+	Xinzuo     string
+	House      string
+	Car        string
+}
