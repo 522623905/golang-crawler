b7ebd671958c2f4cad5438d59fe29d4a1ea2a4ed
long <522623905@qq.com>
2018-05-31 16:42:23 +0800

    更多城市，增加了爬取下一页功能

--- crawler-queue/main.go
+++ crawler-queue/main.go
@@ -11,9 +11,12 @@ func main() {
 		Scheduler:   &scheduler.QueuedScheduler{},
 		WorkerCount: 100,
 	}
+	//	e.Run(engine.Request{
+	//		Url:       "http://www.zhenai.com/zhenghun",
+	//		ParseFunc: parser.ParseCityList,
+	//	})
 	e.Run(engine.Request{
-		Url:       "http://www.zhenai.com/zhenghun",
-		ParseFunc: parser.ParseCityList,
+		Url:       "http://www.zhenai.com/zhenghun/nanning",
+		ParseFunc: parser.ParseCity,
 	})
-
 }
--- crawler-queue/zhenai/parser/city.go
+++ crawler-queue/zhenai/parser/city.go
@@ -10,12 +10,15 @@ import (
 
 //获取某城市内的人的（url+名字）表达式，如：
 //<a href="http://album.zhenai.com/u/1188668898" target="_blank">交友征婚</a>
-const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+//第3页的信息，如：http://www.zhenai.com/zhenghun/shanghai/3
+var (
+	profileRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+	cityUrlRe = regexp.MustCompile(`href="(http://www.zhenai.com/zhenghun/[^"]+)"`) //看到更多的城市信息,如下一页
+)
 
 //在某城市中，解析出人的url和name
 func ParseCity(contents []byte) engine.ParseResult {
-	re, _ := regexp.Compile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	//	limit := 10 //限制人的个数
 	for _, m := range matches {
@@ -34,6 +37,14 @@ func ParseCity(contents []byte) engine.ParseResult {
 		//		if limit == 0 {
 		//			break
 		//		}
+	}
+	//看到更多的城市信息
+	matches = cityUrlRe.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests, engine.Request{
+			Url:       string(m[1]),
+			ParseFunc: ParseCity,
+		})
 	}
 	return result
 }
